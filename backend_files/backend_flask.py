b955084a5dd83ec8a128f6333b65600b48028fa8
AyeshaKhokhar <ayesharasheed6949@gmail.com>
2024-07-30 20:31:20 +0500

    Connecting to database

--- backend_files/backend_flask.py
+++ backend_files/backend_flask.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+import mysql.connector
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="aishamysql2023",
+        database="hotel_handling"
+    )
