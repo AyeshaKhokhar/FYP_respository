1f5a92fce77dce3a2bdb05c4d3979e2b59c230ea
AyeshaKhokhar <ayesharasheed6949@gmail.com>
2024-08-02 16:32:22 +0500

    fetching all data from db

--- backend_files/backend_flask.py
+++ backend_files/backend_flask.py
@@ -14,3 +14,22 @@ def get_db_connection():
         password="aishamysql2023",
         database="hotel_handling"
     )
+
+def fetch_all_data():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+        
+    hotel_details = []
+    cursor.execute('''
+        SELECT hotel_name, hotel_city, review_score, hotel_price, hotel_pic
+        FROM hotel_info
+        ''')
+    result = cursor.fetchall()
+    if result:
+        hotel_details.extend(result)
+        
+    cursor.close()
+    connection.close()
+         
+    print("Sending response:", hotel_details)  # Debug response data
+    return hotel_details
